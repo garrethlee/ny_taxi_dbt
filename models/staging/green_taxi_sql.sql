820cf0fa51265bbb2a4222183d4951cd55f7ef68
garrethlee20@gmail.com <garrethlee20@gmail.com>
2023-04-03 01:03:54 +0000

    Created staging and core data models

---
+++ models/staging/green_taxi_sql.sql
@@ -0,0 +1,48 @@
+{{ config(materialized="view") }}
+
+with tripdata as (
+    select *,
+    row_number() over(partition by vendorid, lpep_pickup_datetime) rn
+    from {{ source("staging", "green_taxi_data") }}
+)
+
+select 
+
+    -- identifiers
+    -- generates unique primary key based on 2 or more columns 
+    {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
+
+    cast(vendorid as integer) as vendorid,
+    cast(ratecodeid as integer) as ratecodeid,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    
+    -- timestamps
+    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    store_and_fwd_flag,
+    cast(passenger_count as integer) as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    cast(payment_type as integer) as payment_type,
+    {{ get_payment_description('payment_type') }} as payment_description,
+    cast(congestion_surcharge as numeric) as congestion_surcharge
+
+from tripdata
+where vendorid is not null and rn = 1
+
+{% if var('is_test', default = true) %}
+
+limit 100
+
+{% endif %}
