f1a7667870709ffce4d040a5b677edff19cf58de
garrethlee20@gmail.com <garrethlee20@gmail.com>
2023-04-04 21:02:25 +0000

    changed if expressions

--- models/staging/green_taxi_sql.sql
+++ models/staging/green_taxi_sql.sql
@@ -1,31 +1,31 @@
 {{ config(materialized="view") }}
 
-with tripdata as (
-    select *,
-    row_number() over(partition by vendorid, lpep_pickup_datetime) rn
-    from {{ source("staging", "green_taxi_data") }}
-)
+with
+    tripdata as (
+        select *, row_number() over (partition by vendorid, lpep_pickup_datetime) rn
+        from {{ source("staging", "green_taxi_data") }}
+    )
 
-select 
+select
 
     -- identifiers
     -- generates unique primary key based on 2 or more columns 
-    {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
+    {{ dbt_utils.surrogate_key(["vendorid", "lpep_pickup_datetime"]) }} as tripid,
 
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
-    cast(pulocationid as integer) as  pickup_locationid,
+    cast(pulocationid as integer) as pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
-    
+
     -- timestamps
     cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
     cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
-    
+
     -- trip info
     store_and_fwd_flag,
     cast(passenger_count as integer) as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    
+
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
     cast(extra as numeric) as extra,
@@ -35,14 +35,10 @@ select
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     cast(payment_type as integer) as payment_type,
-    {{ get_payment_description('payment_type') }} as payment_description,
+    {{ get_payment_description("payment_type") }} as payment_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
 from tripdata
 where vendorid is not null and rn = 1
 
-{% if env_var('DBT_IS_TEST') %}
-
-limit 100
-
-{% endif %}
+{% if env_var("DBT_IS_TEST") == "true" %} limit 100 {% endif %}
